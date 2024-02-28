9804099c5523ff9bc39d4bcfe3d43f6dfda02d10
Pablo Rubiano <pcrubianoa@users.noreply.github.com>
2024-02-28 17:37:52 -0500

    WIP Drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,18 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Slot, SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
 import { useColorScheme } from "react-native";
+
+import React, { useEffect } from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { View } from '@/components/Themed';
+import { Drawer } from 'expo-router/drawer';
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { Feather, EvilIcons, SimpleLineIcons, AntDesign  } from '@expo/vector-icons';
+import { router, usePathname } from 'expo-router';
+import { useFonts } from "expo-font";
+import CustomDrawerContent from '@/components/CustomDrawerContent';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -53,8 +61,63 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <SessionProvider>
-        <Slot />
+      <Drawer
+      screenOptions={{
+        drawerLabelStyle: {
+          marginLeft: -20
+        }
+       }}
+       drawerContent={ CustomDrawerContent }>
+      <Drawer.Screen
+        name="pedidos"
+        options={{
+          drawerLabel: 'Pedidos',
+          title: 'PEDIDOS',
+          drawerIcon: ({ focused, color, size }) => (
+            <AntDesign name="pptfile1" size={24} color="black" />
+          ),
+         }}>
+      </Drawer.Screen>
+      <Drawer.Screen
+        name="sincronizacion"
+        options={{
+          drawerLabel: 'Sincronización',
+          title: 'SINCRONIZACIÓN',
+          drawerActiveTintColor: 'lightgray',
+          drawerIcon: ({ focused, color, size }) => (
+            <AntDesign name="retweet" size={24} color="black" />
+          ),
+         }}>
+      </Drawer.Screen>
+      <Drawer.Screen
+        name="terminos"
+        options={{
+          drawerLabel: 'Terminos y condiciones',
+          title: 'TERMINOS Y CONDICIONES',
+          drawerActiveTintColor: 'lightgray',
+          drawerIcon: ({ focused, color, size }) => (
+            <AntDesign name="retweet" size={24} color="black" />
+          ),
+         }}>
+      </Drawer.Screen>
+    </Drawer>
       </SessionProvider>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  profile:{
+    margin: 20,
+    flex: 1,
+  },
+  navItemLabel: {
+    //marginLeft: -20,
+    //fontSize: 18
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
